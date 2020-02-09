a4a5531899c8aa540e9dddeadbc793082a61580c
S-Yamamoto3215 <yamamotoshoya8@gmail.com>
2020-02-09 22:22:22 +0900

    setting zsh

--- dot.zsh/environment.sh
+++ dot.zsh/environment.sh
@@ -6,3 +6,16 @@ export COPYFILE_DISABLE=1
 
 # Limit
 limit coredumpsize 0
+
+# ビープ音を鳴らさないようにする
+setopt no_beep
+
+# ディレクトリにマッチした場合 末尾に / を付加
+setopt mark_dirs
+
+# 自動補完を有効にする
+autoload -Uz compinit ; compinit
+# 補完の選択を楽にする
+zstyle ':completion:*' menu select
+# 補完候補をできるだけ詰めて表示する
+setopt list_packed
--- dot.zsh/history.sh
+++ dot.zsh/history.sh
@@ -1,6 +1,8 @@
 HISTFILE=~/.zsh_history
 HISTSIZE=1000
 SAVEHIST=100000
+setopt histignorealldups
 setopt hist_ignore_dups
 setopt hist_ignore_space
 setopt share_history
+setopt hist_verify
