3d5b6c80fb49f4dd2e1d3dbd6676cc4d90f0a741
S-Yamamoto3215 <yamamotoshoya8@gmail.com>
2020-02-28 00:12:44 +0900

    history setting, add peco

--- dot.zsh/history.sh
+++ dot.zsh/history.sh
@@ -1,8 +1,19 @@
 HISTFILE=~/.zsh_history
 HISTSIZE=1000
 SAVEHIST=100000
-setopt histignorealldups
-setopt hist_ignore_dups
-setopt hist_ignore_space
-setopt share_history
-setopt hist_verify
+setopt hist_ignore_space # 先頭に半角スペースは無視
+setopt hist_reduce_blanks # 余分な空白は詰める
+setopt hist_ignore_all_dups # 重複するコマンド行は古い方を削除
+setopt hist_no_store # historyは履歴に登録しない
+setopt share_history # コマンド履歴ファイルを共有する
+setopt hist_expand # 補完時にヒストリを自動的に展開する。
+setopt hist_verify # 一旦編集可能
+
+# pecoの設定 ※ brew install peco をする
+function peco-history-selection() {
+    BUFFER=`history -n 1 | tail -r  | awk '!a[$0]++' | peco`
+    CURSOR=$#BUFFER
+    zle reset-prompt
+}
+zle -N peco-history-selection
+bindkey '^R' peco-history-selection
