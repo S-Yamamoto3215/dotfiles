800b0134a594d3e323dbd5630e4d72305e827419
S-Yamamoto3215 <yamamotoshoya8@gmail.com>
2020-05-24 15:11:28 +0900

    setting trash-put alias

--- dot.zsh/alias.sh
+++ dot.zsh/alias.sh
@@ -4,6 +4,16 @@ alias cp='cp -i'
 alias mv='mv -i'
 alias ll="ls -lF"
 alias DS-delete="find ./ -name '.DS_Store' -exec rm -f {} \;"
+
+# trash-put
+if type trash-put &> /dev/null
+then
+  alias rm='trash-put'
+fi
+alias rm-l='trash-list'
+alias rm-re='trash-restore'
+alias rm-empty='trash-empty'
+
 # bundler
 alias be='bundle exec'
 alias binst='bundle install'
